f14c5beb24f5869c03d20f608a59f4d1f23b45d8
Emano Waldeck <emano.waldeck@gmail.com>
2024-04-06 03:58:33 -0400

    version 0.1.2

--- data/window/index.js
+++ data/window/index.js
@@ -6,65 +6,73 @@ const proceed = file => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   image.onload = async () => {
-    const {AutoModel, AutoProcessor, env, RawImage} = await import('./transformers/transformers.js');
-    env.allowRemoteModels = false;
-    env.allowLocalModels = true;
-    env.localModelPath = new URL('models', location.href).href;
-    env.backends.onnx.wasm.wasmPaths = new URL('transformers', location.href).href + '/';
-    env.backends.onnx.wasm.proxy = true;
-    env.backends.onnx.wasm.numThreads = 1;
+    try {
+      const {AutoModel, AutoProcessor, env, RawImage} = await import('./transformers/transformers.js');
+      env.allowRemoteModels = false;
+      env.allowLocalModels = true;
+      env.localModelPath = new URL('models', location.href).href;
+      env.backends.onnx.wasm.wasmPaths = new URL('transformers', location.href).href + '/';
+      env.backends.onnx.wasm.proxy = true;
+      env.backends.onnx.wasm.numThreads = 1;
 
-    const oImage = await RawImage.fromBlob(image);
-    const {width, height} = oImage;
+      const oImage = await RawImage.fromBlob(image);
+      const {width, height} = oImage;
 
-    canvas.width = width;
-    canvas.height = height;
+      canvas.width = width;
+      canvas.height = height;
 
-    ctx.font = '5vw Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText('Please wait...', width / 2, height / 2);
+      ctx.font = '5vw Arial';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText('Please wait...', width / 2, height / 2);
 
-    const model = await AutoModel.from_pretrained('briaai/RMBG-1.4', {
-      config: {
-        model_type: 'custom'
-      },
-      quantized: false,
-      device: 'webgpu'
-    });
-    const processor = await AutoProcessor.from_pretrained('briaai/RMBG-1.4', {
-      config: {
-        do_normalize: !0,
-        do_pad: !1,
-        do_rescale: !0,
-        do_resize: !0,
-        image_mean: [.5, .5, .5],
-        feature_extractor_type: 'ImageFeatureExtractor',
-        image_std: [1, 1, 1],
-        resample: 2,
-        rescale_factor: .00392156862745098,
-        size: {
-          width: 1024,
-          height: 1024
+      const model = await AutoModel.from_pretrained('briaai/RMBG-1.4', {
+        config: {
+          model_type: 'custom'
+        },
+        quantized: false,
+        device: 'webgpu'
+      });
+      const processor = await AutoProcessor.from_pretrained('briaai/RMBG-1.4', {
+        config: {
+          do_normalize: !0,
+          do_pad: !1,
+          do_rescale: !0,
+          do_resize: !0,
+          image_mean: [.5, .5, .5],
+          feature_extractor_type: 'ImageFeatureExtractor',
+          image_std: [1, 1, 1],
+          resample: 2,
+          rescale_factor: .00392156862745098,
+          size: {
+            width: 1024,
+            height: 1024
+          }
         }
+      });
+
+      const {pixel_values: n} = await processor(oImage);
+      const {output: d} = await model({
+        input: n
+      });
+      const rImage = await RawImage.fromTensor(d[0].mul(255).to('uint8')).resize(width, height);
+
+      ctx.drawImage(oImage.toCanvas(), 0, 0);
+
+      const p = ctx.getImageData(0, 0, width, height);
+      for (let m = 0; m < rImage.data.length; ++m) {
+        p.data[4 * m + 3] = rImage.data[m];
       }
-    });
+      ctx.putImageData(p, 0, 0);
 
-    const {pixel_values: n} = await processor(oImage);
-    const {output: d} = await model({
-      input: n
-    });
-    const rImage = await RawImage.fromTensor(d[0].mul(255).to('uint8')).resize(width, height);
-
-    ctx.drawImage(oImage.toCanvas(), 0, 0);
-
-    const p = ctx.getImageData(0, 0, width, height);
-    for (let m = 0; m < rImage.data.length; ++m) {
-      p.data[4 * m + 3] = rImage.data[m];
+      self.download.disabled = false;
     }
-    ctx.putImageData(p, 0, 0);
-
-    self.download.disabled = false;
+    catch (e) {
+      console.error(e);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillText('Error!', canvas.width / 2, canvas.height / 2);
+      alert(e.message);
+    }
   };
 
   self.download.disabled = true;
@@ -80,7 +88,7 @@ document.ondragover = e => {
   e.dataTransfer.dropEffect = 'copy';
 };
 document.ondrop = e => {
-  event.preventDefault();
+  e.preventDefault();
   const files = e.dataTransfer.files;
   proceed(files[0]);
 };
